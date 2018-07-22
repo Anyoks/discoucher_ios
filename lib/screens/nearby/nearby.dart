62dca55b6987e634b24edf0211b444f09e2a4ff2
Dennis Riungu <denpalrius@gmail.com>
2018-07-22 16:58:49 +0300

    Add location package

--- lib/screens/nearby/nearby.dart
+++ lib/screens/nearby/nearby.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:location/location.dart';
 
 class NearbyPage extends StatefulWidget {
   @override
@@ -6,8 +7,40 @@ class NearbyPage extends StatefulWidget {
 }
 
 class _NearbyPageState extends State<NearbyPage> {
+  var currentLocation = <String, double>{};
+  
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  // getLocation(){
+  //   var location = new Location();
+  //   try {
+  //     currentLocation = await location.getLocation;
+  //   } on PlatformException {
+  //     currentLocation = null;
+  //   }
+
+  //   location.onLocationChanged.listen((Map<String,double> currentLocation) {
+  //     print(currentLocation["latitude"]);
+  //     print(currentLocation["longitude"]);
+  //     print(currentLocation["accuracy"]);
+  //     print(currentLocation["altitude"]);
+  //     print(currentLocation["speed"]);
+  //     print(currentLocation["speed_accuracy"]); // Will always be 0 on iOS
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Nerby")));
+
+
+    return new Container();
   }
 }
