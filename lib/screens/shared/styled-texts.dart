104dc93123b32022589cfd9e8cc3620fff8f4aaf
Dennis Riungu <denpalrius@gmail.com>
2018-09-20 04:16:50 +0300

    Add styled Text and TextSpan

---
+++ lib/screens/shared/styled-texts.dart
@@ -0,0 +1,19 @@
+import 'package:discoucher/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+normalText(text) {
+  return TextSpan(text: text, style: TextStyle(fontSize: 16.0));
+}
+
+greenText(text) {
+  return TextSpan(
+      text: text, style: TextStyle(fontSize: 16.0, color: xDiscoucherGreen));
+}
+
+boldGreenText(text) {
+  return Text(
+    text,
+    style: TextStyle(
+        color: xDiscoucherGreen, fontWeight: FontWeight.bold, fontSize: 18.0),
+  );
+}
