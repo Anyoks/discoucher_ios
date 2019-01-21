77ba89a583a21a316b9b0c74c02681541064c647
Denn Orina <anyoksdenn@gmail.com>
2019-01-21 22:37:43 +0300

    increased height and added close button

--- lib/screens/details/redeem-dialog.dart
+++ lib/screens/details/redeem-dialog.dart
@@ -9,12 +9,29 @@ showRedeemDialog(BuildContext context, Voucher voucher) {
     builder: (context) {
       return AlertDialog(
         content: Container(
-          width: MediaQuery.of(context).size.width * 0.9,
-          height: MediaQuery.of(context).size.height * 0.5,
+          width: MediaQuery.of(context).size.width, //* 0.9,
+          height:  MediaQuery.of(context).size.height * 0.6,
           child: ListView(
             children: <Widget>[
               RedeemPage(
                 voucher: voucher,
+              ),
+              Container(
+                alignment: Alignment(1.0, 1.0),
+                child: FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      'Close',
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 17.0),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
