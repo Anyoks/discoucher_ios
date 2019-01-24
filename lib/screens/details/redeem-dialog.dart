3d4243d6d2ed64e35c8d724d2b46d579999273bd
Denn Orina <anyoksdenn@gmail.com>
2019-01-24 13:10:48 +0300

    added close button at the bottom

--- lib/screens/details/redeem-dialog.dart
+++ lib/screens/details/redeem-dialog.dart
@@ -5,33 +5,33 @@ import 'package:flutter/material.dart';
 
 showRedeemDialog(BuildContext context, Voucher voucher) {
   showDialog(
+    //  barrierDismissible: true,
+
     context: context,
     builder: (context) {
       return AlertDialog(
+        actions: [
+          FlatButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                'Close',
+                style: TextStyle(
+                    color: Theme.of(context).primaryColor, fontSize: 17.0),
+              ),
+            ),
+          ),
+        ],
         content: Container(
           width: MediaQuery.of(context).size.width, //* 0.9,
-          height:  MediaQuery.of(context).size.height * 0.6,
+          height: MediaQuery.of(context).size.height * 0.6,
           child: ListView(
             children: <Widget>[
               RedeemPage(
                 voucher: voucher,
-              ),
-              Container(
-                alignment: Alignment(1.0, 1.0),
-                child: FlatButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Text(
-                      'Close',
-                      style: TextStyle(
-                          color: Theme.of(context).primaryColor,
-                          fontSize: 17.0),
-                    ),
-                  ),
-                ),
               ),
             ],
           ),
