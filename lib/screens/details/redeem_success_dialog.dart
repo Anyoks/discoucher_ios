1a198b9ad8a2aadee08a4db086aeff1987174d48
Denn Orina <anyoksdenn@gmail.com>
2019-01-24 16:11:12 +0300

    Now dialogs are in a singlelist child view, more hieght friendlt

--- lib/screens/details/redeem_success_dialog.dart
+++ lib/screens/details/redeem_success_dialog.dart
@@ -4,18 +4,40 @@ import 'package:flutter/material.dart';
 
 showSuccessRedeemDialog(BuildContext context, Voucher voucher) {
   showDialog(
+    barrierDismissible: false, // force users to review!
     context: context,
     builder: (context) {
       return AlertDialog(
-        content: Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            height: MediaQuery.of(context).size.height * 0.6,
-            child: ListView(children: <Widget>[
-              SuccessfulRedemptionPage(
-                voucher: voucher,
-              ),
-            ])),
-      );
+          // actions: [
+          //     FlatButton(
+          //       onPressed: () {
+          //         Navigator.of(context).pop();
+          //       },
+          //       child: Padding(
+          //         padding: EdgeInsets.all(10.0),
+          //         child: Text(
+          //           'Close',
+          //           style: TextStyle(
+          //               color: Theme.of(context).primaryColor, fontSize: 17.0),
+          //         ),
+          //       ),
+          //     ),
+          //   ],
+          content: SingleChildScrollView(
+              child: ListBody(children: <Widget>[
+        SuccessfulRedemptionPage(
+          voucher: voucher,
+        ),
+      ]))
+          // Container(
+          //     width: MediaQuery.of(context).size.width * 0.9,
+          //     height: MediaQuery.of(context).size.height * 0.6,
+          //     child: ListView(children: <Widget>[
+          //       SuccessfulRedemptionPage(
+          //         voucher: voucher,
+          //       ),
+          //     ])),
+          );
     },
   );
 }
